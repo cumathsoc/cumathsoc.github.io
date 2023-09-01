66f90d2f320fb19253fb20df01892775ef80dc1f
Ju Hong Kim <nabimoon1234@gmail.com>
2023-08-31 23:40:31 -0400

    Update BBQ event and change website title

--- scripts/data/events.js
+++ scripts/data/events.js
@@ -20,6 +20,20 @@ let events = [
 },
 {
   "type": "news-poster",
+  "title": "BBQ & Book SALE",
+  "presentor": "",
+  "date": "Thursday Sept. 7 2023 (10am-5:30pm)",
+  "location": "In front of Herzberg (HP)",
+  "desc": "Come to our student BBQ and buy math books. You can also get your Math & Stat Society membership which comes with free food.",
+  "img": "images/bbq-poster.png",
+  "calendar": {
+      "google": "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20230907T140000Z%2F20230907T213000Z&details=%2A%20BBQ%20from%2011am-4pm%0A%2A%20Purchase%20membership%0A%2A%20Buy%20Math%20books&location=Outside%20of%20Herzberg&text=MathSoc%20-%20BBQ%20%26%20Book%20Sale",
+      "outlook": "https://outlook.live.com/calendar/0/action/compose?body=%2A%20BBQ%20from%2011am-4pm%0A%2A%20Purchase%20membership%0A%2A%20Buy%20Math%20books&enddt=2023-09-07T21%3A30%3A00%2B00%3A00&location=Outside%20of%20Herzberg&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2023-09-07T14%3A00%3A00%2B00%3A00&subject=MathSoc%20-%20BBQ%20%26%20Book%20Sale",
+      "apple": "data:text/calendar;charset=utf8,BEGIN:VCALENDAR%0AVERSION:2.0%0ABEGIN:VEVENT%0ADTSTART:20230907T140000Z%0ADTEND:20230907T213000Z%0ASUMMARY:MathSoc%20-%20BBQ%20%26%20Book%20Sale%0ADESCRIPTION:*%20BBQ%20from%2011am-4pm%5Cn*%20Purchase%20membership%5Cn*%20Buy%20Math%20books%0ALOCATION:Outside%20of%20Herzberg%0AEND:VEVENT%0AEND:VCALENDAR%0A"
+  } 
+},
+{
+  "type": "news-poster",
   "title": "MathSoc @ Club Expo",
   "presentor": "",
   "date": "Tuesday September 5",
