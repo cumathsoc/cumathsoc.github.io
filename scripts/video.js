2a678048c1b3e9f426cf6e162b4a443093505453
Ju Hong Kim <nabimoon1234@gmail.com>
2023-09-03 18:43:26 -0400

    [#7] Add Login Video page

--- scripts/video.js
+++ scripts/video.js
@@ -1,21 +1,32 @@
 const getVideos = async() => {
-  let url = 'http://68.233.123.145/verify?id=100996883';
+  let value = document.getElementById('stud-id').value;
+
+  console.log(value);
+  let url = 'https://math-soc.uk/verify?id=' + value;
   fetch(url).then(res => {
     if (!res.ok) {
       throw new Error();
     }
     return res.json();
   }).then(data => {
-    if (data.length == 0 || !("list" in data)) {
+    if (data.length == 0 || !("list" in data) || "error" in data && data.error) {
+      document.getElementById('login-error').style.display = 'block';
       return;
     }
     videos.list = data.list;
+    showVideos();
     updatePage(videos.list);
     return;
   }).catch(err => {
     console.error('Fetch error');
   });
 };
+
+const showVideos = () => {
+  document.getElementById('video-login').style.display = "None";
+  document.getElementById('current-video').style.display = "block";
+  document.getElementById('prev-video').style.display = "block";
+}
 
 const switchVideo = (index, scroll) => {
   let video = videos.list[index];
