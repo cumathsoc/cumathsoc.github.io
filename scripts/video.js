ee83c203d7dae61b1fce74d03398435f80a48308
Ju Hong Kim <nabimoon1234@gmail.com>
2023-09-03 14:20:46 -0400

    [#6] Display Videos from playlist

---
+++ scripts/video.js
@@ -0,0 +1,38 @@
+const getVideos = async() => {
+  let url = 'http://68.233.123.145/verify?id=100996883';
+  fetch(url).then(res => {
+    if (!res.ok) {
+      throw new Error();
+    }
+    return res.json();
+  }).then(data => {
+    if (data.length == 0 || !("list" in data)) {
+      return;
+    }
+    videos.list = data.list;
+    updatePage(videos.list);
+    return;
+  }).catch(err => {
+    console.error('Fetch error');
+  });
+};
+
+const switchVideo = (index) => {
+  let video = videos.list[index];
+  document.getElementById('video-title').innerHTML = video.title;
+  document.getElementById('video-desc').innerHTML = video.desc;
+  document.getElementById('video-iframe').src = 'https://www.youtube-nocookie.com/embed/' + video.videoId + "?controls=0&origin='cumathsoc.ca'";
+};
+
+const getListFmt = (list) => {
+  return list.map((item, index, list) => {
+    return "<p onclick=switchVideo('" + index + "')>" + item.title + '</p>'; //onclick and pass videoid
+  }).join('');
+};
+
+const updatePage = (list) => {
+  console.log(list);
+  document.getElementById('latest-video-title').innerHTML = list[0].title;
+  switchVideo(0);
+  document.getElementById('list-videos').innerHTML = getListFmt(list);
+};
