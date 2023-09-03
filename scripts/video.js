387e6a77b1993d6b96d5f4c7a64f1dd2bcd5c089
Ju Hong Kim <nabimoon1234@gmail.com>
2023-09-03 14:56:53 -0400

    [#6] Enable video fullscreen and focus on video after video swap

--- scripts/video.js
+++ scripts/video.js
@@ -17,22 +17,25 @@ const getVideos = async() => {
   });
 };
 
-const switchVideo = (index) => {
+const switchVideo = (index, scroll) => {
   let video = videos.list[index];
   document.getElementById('video-title').innerHTML = video.title;
   document.getElementById('video-desc').innerHTML = video.desc;
-  document.getElementById('video-iframe').src = 'https://www.youtube-nocookie.com/embed/' + video.videoId + "?controls=0&origin='cumathsoc.ca'";
+  document.getElementById('video-iframe').src = 'https://www.youtube-nocookie.com/embed/' + video.videoId + "?origin='cumathsoc.ca'";
+  if (scroll) {
+    document.getElementById('display-video').scrollIntoView(true);
+  }
 };
 
 const getListFmt = (list) => {
   return list.map((item, index, list) => {
-    return "<p onclick=switchVideo('" + index + "')>" + item.title + '</p>'; //onclick and pass videoid
+    return "<p class = 'video-item' onclick=switchVideo(" + index + ',true)>' + item.title + '</p>'; //onclick and pass videoid
   }).join('');
 };
 
 const updatePage = (list) => {
   console.log(list);
   document.getElementById('latest-video-title').innerHTML = list[0].title;
-  switchVideo(0);
+  switchVideo(0, false);
   document.getElementById('list-videos').innerHTML = getListFmt(list);
 };
