30d157204deb1699b93e89ea98844f5ecd49d477
Ju Hong Kim <nabimoon1234@gmail.com>
2023-09-26 09:55:39 -0400

    [event] Amundsen talk

--- scripts/data/events.js
+++ scripts/data/events.js
@@ -4,13 +4,27 @@ let events = [
   "title": "",
   "presentor": "",
   "date": "",
-  "location": "Herzberg (HP) 4351",
+  "location": "Herzberg (HP) 3422",
   "desc": "",
   "img": "",
   "calendar": {
       "google": "",
       "outlook": "",
       "apple": ""
+  }   
+},
+{
+  "type": "news-poster",
+  "title": "Chaos: Finding Order in Disorder",
+  "presentor": "Dave Amundsen",
+  "date": "Tuesday September 26 2023",
+  "location": "Herzberg (HP) 3422",
+  "desc": "<b>Abstract:</b> From the \"butterfly effect\" of weather prediction, to the vagaries of the stock market and the intricate beauty of snowflakes, examples of chaotic behavior surround us. But what is chaos? Is it a manifestation of some underlying random process? Is it a \"catch all\" for effects which we do not know or cannot resolve? In this presentation, I will talk about the mathematical notions behind chaos, and how it applies to a wide range of everyday phenomena. You will learn that behind the mysterious, seemingly random nature of chaos can lie a rich, intricate structure.  With this comes deeper understanding but such complexity also brings limitations. Through various examples we will explore how order emerges from disorder!",
+  "img": "images/talks/2023-09-26-amundsen.png",
+  "calendar": {
+      "google": "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20230926T200000Z%2F20230926T213000Z&details=From%20the%20%22butterfly%20effect%22%20of%20weather%20prediction%2C%20to%20the%20vagaries%20of%20the%20stock%20market%20and%20the%20intricate%20beauty%20of%20snowflakes%2C%20examples%20of%20chaotic%20behavior%20surround%20us.%20But%20what%20is%20chaos%3F%20Is%20it%20a%20manifestation%20of%20some%20underlying%20random%20process%3F%20Is%20it%20a%20%22catch%20all%22%20for%20effects%20which%20we%20do%20not%20know%20or%20cannot%20resolve%3F%20In%20this%20presentation%2C%20I%20will%20talk%20about%20the%20mathematical%20notions%20behind%20chaos%2C%20and%20how%20it%20applies%20to%20a%20wide%20range%20of%20everyday%20phenomena.%20You%20will%20learn%20that%20behind%20the%20mysterious%2C%20seemingly%20random%20nature%20of%20chaos%20can%20lie%20a%20rich%2C%20intricate%20structure.%20With%20this%20comes%20deeper%20understanding%20but%20such%20complexity%20also%20brings%20limitations.%20Through%20various%20examples%20we%20will%20explore%20how%20order%20emerges%20from%20disorder%21&location=HP3422&text=MathSoc%20Talk%202%20-%20Chaos%3A%20Finding%20Order%20in%20Disorder",
+      "outlook": "https://outlook.office.com/calendar/0/action/compose?body=From%20the%20%22butterfly%20effect%22%20of%20weather%20prediction%2C%20to%20the%20vagaries%20of%20the%20stock%20market%20and%20the%20intricate%20beauty%20of%20snowflakes%2C%20examples%20of%20chaotic%20behavior%20surround%20us.%20But%20what%20is%20chaos%3F%20Is%20it%20a%20manifestation%20of%20some%20underlying%20random%20process%3F%20Is%20it%20a%20%22catch%20all%22%20for%20effects%20which%20we%20do%20not%20know%20or%20cannot%20resolve%3F%20In%20this%20presentation%2C%20I%20will%20talk%20about%20the%20mathematical%20notions%20behind%20chaos%2C%20and%20how%20it%20applies%20to%20a%20wide%20range%20of%20everyday%20phenomena.%20You%20will%20learn%20that%20behind%20the%20mysterious%2C%20seemingly%20random%20nature%20of%20chaos%20can%20lie%20a%20rich%2C%20intricate%20structure.%20With%20this%20comes%20deeper%20understanding%20but%20such%20complexity%20also%20brings%20limitations.%20Through%20various%20examples%20we%20will%20explore%20how%20order%20emerges%20from%20disorder%21&enddt=2023-09-26T21%3A30%3A00%2B00%3A00&location=HP3422&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2023-09-26T20%3A00%3A00%2B00%3A00&subject=MathSoc%20Talk%202%20%28Chaos%20-%20Finding%20Order%20in%20Disorder%29",
+      "apple": "data:text/calendar;charset=utf8,BEGIN:VCALENDAR%0AVERSION:2.0%0ABEGIN:VEVENT%0ADTSTART:20230926T200000Z%0ADTEND:20230926T213000Z%0ASUMMARY:MathSoc%20Talk%202%20-%20Chaos%3A%20Finding%20Order%20in%20Disorder%0ADESCRIPTION:From%20the%20%22butterfly%20effect%22%20of%20weather%20prediction%2C%20to%20the%20vagaries%20of%20the%20stock%20market%20and%20the%20intricate%20beauty%20of%20snowflakes%2C%20examples%20of%20chaotic%20behavior%20surround%20us.%20But%20what%20is%20chaos%3F%20Is%20it%20a%20manifestation%20of%20some%20underlying%20random%20process%3F%20Is%20it%20a%20%22catch%20all%22%20for%20effects%20which%20we%20do%20not%20know%20or%20cannot%20resolve%3F%20In%20this%20presentation%2C%20I%20will%20talk%20about%20the%20mathematical%20notions%20behind%20chaos%2C%20and%20how%20it%20applies%20to%20a%20wide%20range%20of%20everyday%20phenomena.%20You%20will%20learn%20that%20behind%20the%20mysterious%2C%20seemingly%20random%20nature%20of%20chaos%20can%20lie%20a%20rich%2C%20intricate%20structure.%20With%20this%20comes%20deeper%20understanding%20but%20such%20complexity%20also%20brings%20limitations.%20Through%20various%20examples%20we%20will%20explore%20how%20order%20emerges%20from%20disorder!%0ALOCATION:HP3422%0AEND:VEVENT%0AEND:VCALENDAR%0A"
   }   
 },
 {
@@ -26,20 +40,6 @@ let events = [
       "outlook": "",
       "apple": ""
   }
-},
-{
-  "type": "news-poster",
-  "title": "The Pigeonhole Principle: Pigeons, Prime NUmbers and Old Socks: What Do They Have In Common?",
-  "presentor": "Dr. James Bondar",
-  "date": "Tuesday September 19 2023",
-  "location": "Herzberg (HP) 3422",
-  "desc": "It is often said that the best mathematics starts with very simple ideas. This is true of the pigeonhole principle which we first encounter as a riddle which can be solved by elementary school students. The applications of this simple principle lead in many directions: graph theory, the behaviour of billards balls, prime numbers, ... Some of these applications are deep and difficult and others are light and entertaining puzzles. We shall spend most of our time with the latter. Everyone is welcome!",
-  "img": "images/talks/2023-09-19-bondar.jpg",
-  "calendar": {
-      "google": "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20230919T200000Z%2F20230919T213000Z&details=It%20is%20often%20said%20that%20the%20best%20mathematics%20starts%20with%20very%20simple%20ideas.%20This%20is%20true%20of%20the%20pigeonhole%20principle%20which%20we%20first%20encounter%20as%20a%20riddle%20which%20can%20be%20solved%20by%20elementary%20school%20students.%20The%20applications%20of%20this%20simple%20principle%20lead%20in%20many%20directions%3A%20graph%20theory%2C%20the%20behaviour%20of%20billards%20balls%2C%20prime%20numbers%2C%20...%20Some%20of%20these%20applications%20are%20deep%20and%20difficult%20and%20others%20are%20light%20and%20entertaining%20puzzles.%20We%20shall%20spend%20most%20of%20our%20time%20with%20the%20latter.%20Everyone%20is%20welcome%21%0A%0ADr.%20James%20Bondar&location=HP3422&text=MathSoc%20Colloquium%3A%20Pigeonhole%20Principle",
-      "outlook": "https://outlook.office.com/calendar/0/action/compose?body=It%20is%20often%20said%20that%20the%20best%20mathematics%20starts%20with%20very%20simple%20ideas.%20This%20is%20true%20of%20the%20pigeonhole%20principle%20which%20we%20first%20encounter%20as%20a%20riddle%20which%20can%20be%20solved%20by%20elementary%20school%20students.%20The%20applications%20of%20this%20simple%20principle%20lead%20in%20many%20directions%3A%20graph%20theory%2C%20the%20behaviour%20of%20billards%20balls%2C%20prime%20numbers%2C%20...%20Some%20of%20these%20applications%20are%20deep%20and%20difficult%20and%20others%20are%20light%20and%20entertaining%20puzzles.%20We%20shall%20spend%20most%20of%20our%20time%20with%20the%20latter.%20Everyone%20is%20welcome%21%0A%0ADr.%20James%20Bondar&enddt=2023-09-19T21%3A30%3A00%2B00%3A00&location=HP3422&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2023-09-19T20%3A00%3A00%2B00%3A00&subject=MathSoc%20Colloquium%3A%20Pigeonhole%20Principle",
-      "apple": "data:text/calendar;charset=utf8,BEGIN:VCALENDAR%0AVERSION:2.0%0ABEGIN:VEVENT%0ADTSTART:20230919T200000Z%0ADTEND:20230919T213000Z%0ASUMMARY:MathSoc%20Colloquium%3A%20Pigeonhole%20Principle%0ADESCRIPTION:It%20is%20often%20said%20that%20the%20best%20mathematics%20starts%20with%20very%20simple%20ideas.%20This%20is%20true%20of%20the%20pigeonhole%20principle%20which%20we%20first%20encounter%20as%20a%20riddle%20which%20can%20be%20solved%20by%20elementary%20school%20students.%20The%20applications%20of%20this%20simple%20principle%20lead%20in%20many%20directions%3A%20graph%20theory%2C%20the%20behaviour%20of%20billards%20balls%2C%20prime%20numbers%2C%20...%20Some%20of%20these%20applications%20are%20deep%20and%20difficult%20and%20others%20are%20light%20and%20entertaining%20puzzles.%20We%20shall%20spend%20most%20of%20our%20time%20with%20the%20latter.%20Everyone%20is%20welcome!%5Cn%5CnDr.%20James%20Bondar%0ALOCATION:HP3422%0AEND:VEVENT%0AEND:VCALENDAR%0A"
-  }   
 },
 {
   "type": "news-poster",
