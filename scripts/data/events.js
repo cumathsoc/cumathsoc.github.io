defb67c8b982c02d840f682a4bb095503ec26c69
Ju Hong Kim <nabimoon1234@gmail.com>
2023-09-26 11:45:36 -0400

    [event] Pancake event oct 3

--- scripts/data/events.js
+++ scripts/data/events.js
@@ -15,6 +15,20 @@ let events = [
 },
 {
   "type": "news-poster",
+  "title": "October Pancake Breakfast",
+  "presentor": "",
+  "date": "Tuesday October 3 2023",
+  "location": "Outside Herzberg (HP) 3422",
+  "desc": "We will also be holding our first pancake breakfast next week on Tuesday October 3rd from 10 AM to 2 PM right outside the Math Tutorial Centre (HP 3422). Come join us for some delicious pancakes with your friends and fellow members!",
+  "img": "images/events/pancake-oct.png",
+  "calendar": {
+      "google": "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20231003T140000Z%2F20231003T180000Z&details=We%20will%20also%20be%20holding%20our%20first%20pancake%20breakfast%20next%20week%20on%20Tuesday%20October%203rd%20from%2010%20AM%20to%202%20PM%20right%20outside%20the%20Math%20Tutorial%20Centre%20%28HP%203422%29.%20Come%20join%20us%20for%20some%20delicious%20pancakes%20with%20your%20friends%20and%20fellow%20members%21&location=Outside%20of%20HP3422&text=MathSoc%20Talk%202%20-%20October%20Pancake%20Breakfast",
+      "outlook": "https://outlook.office.com/calendar/0/action/compose?body=We%20will%20also%20be%20holding%20our%20first%20pancake%20breakfast%20next%20week%20on%20Tuesday%20October%203rd%20from%2010%20AM%20to%202%20PM%20right%20outside%20the%20Math%20Tutorial%20Centre%20%28HP%203422%29.%20Come%20join%20us%20for%20some%20delicious%20pancakes%20with%20your%20friends%20and%20fellow%20members%21&enddt=2023-10-03T18%3A00%3A00%2B00%3A00&location=Outside%20of%20HP3422&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2023-10-03T14%3A00%3A00%2B00%3A00&subject=MathSoc%20Talk%202%20-%20October%20Pancake%20Breakfast",
+      "apple": "data:text/calendar;charset=utf8,BEGIN:VCALENDAR%0AVERSION:2.0%0ABEGIN:VEVENT%0ADTSTART:20231003T140000Z%0ADTEND:20231003T180000Z%0ASUMMARY:MathSoc%20Talk%202%20-%20October%20Pancake%20Breakfast%0ADESCRIPTION:We%20will%20also%20be%20holding%20our%20first%20pancake%20breakfast%20next%20week%20on%20Tuesday%20October%203rd%20from%2010%20AM%20to%202%20PM%20right%20outside%20the%20Math%20Tutorial%20Centre%20(HP%203422).%20Come%20join%20us%20for%20some%20delicious%20pancakes%20with%20your%20friends%20and%20fellow%20members!%0ALOCATION:Outside%20of%20HP3422%0AEND:VEVENT%0AEND:VCALENDAR%0A"
+  }
+},
+{
+  "type": "news-poster",
   "title": "Chaos: Finding Order in Disorder",
   "presentor": "Dave Amundsen",
   "date": "Tuesday September 26 2023",
