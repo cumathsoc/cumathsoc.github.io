803bd3225a01eadc68e8cf42921bc5a95d90ebd5
Ju Hong Kim <nabimoon1234@gmail.com>
2023-09-27 22:22:36 -0400

    [news] fix typo

--- scripts/data/events.js
+++ scripts/data/events.js
@@ -19,7 +19,7 @@ let events = [
   "presentor": "Colin Ingalls",
   "date": "October 03 2023",
   "location": "Herzberg (HP) 3422",
-  "desc": "<b>Abstract:</b> Often we have a situation where we have some matrices that we can write in a nicer way by different choosing bases. We will show how quivers (directed graphs) and their path algebras can be used to solvesome of these problems.",
+  "desc": "<b>Abstract:</b> Often we have a situation where we have some matrices that we can write in a nicer way by different choosing bases. We will show how quivers (directed graphs) and their path algebras can be used to solve some of these problems.",
   "img": "images/talks/2023-10-03-ingalls.png",
   "calendar": {
       "google": "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20231003T200000Z%2F20231003T213000Z&details=Presenter%3A%20Colin%20Ingalls%0AAbstract%3A%20Often%20we%20have%20a%20situation%20where%20we%20have%20some%20matrices%20that%20we%20can%20write%20in%20a%20nicer%20way%20by%20different%20choosing%20bases.%20We%20will%20show%20how%20quivers%20%28directed%20graphs%29%20and%20their%20path%20algebras%20can%20be%20used%20to%20solve%5EZsome%20of%20these%20problems.&location=HP3422&text=MathSoc%20-%20Solving%20Matrix%20Problems%20with%20Quivers",
